0111e0553fd101255133ceb39f45f594afcf2f4a
Renê Lima <rene.exe@gmail.com>
2023-09-18 14:55:52 -0300

    Starting the first atom component as Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-between p-24"
+      className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-500"
       data-testid="main"
     >
       <h1>Just start to 🚀</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
   content: [
@@ -12,6 +13,17 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        transparent: 'transparent',
+        'primary': {
+          DEFAULT: colors.blue[500],
+        },
+        'secondary': {
+          DEFAULT: colors.orange[500],
+        },
+        'white': colors.neutral[50],
+        'black': colors.zinc[950],
       },
     },
   },
