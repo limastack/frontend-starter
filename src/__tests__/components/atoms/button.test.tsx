ddb9d5f881be2444a4d9170a80adcba3c9e01aa7
Renê Lima <rene.exe@gmail.com>
2023-09-18 20:51:50 -0300

    Improving Button tests

--- src/__tests__/components/atoms/button.test.tsx
+++ src/__tests__/components/atoms/button.test.tsx
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
+import React from "react";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { Button } from "@/components/atoms/button";
@@ -6,14 +7,14 @@ import { Button } from "@/components/atoms/button";
 describe("Button", () => {
   it("renders with default bg-primary", () => {
     render(<Button>Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("bg-primary");
   });
 
   it("renders with outline variant", () => {
     render(<Button variant="outline">Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("border");
     expect(button).toHaveClass("border-input");
@@ -21,14 +22,14 @@ describe("Button", () => {
 
   it("renders with secondary variant bg", () => {
     render(<Button variant="secondary">Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("bg-secondary");
   });
 
   it("renders with sm size", () => {
     render(<Button size="sm">Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("h-9");
     expect(button).toHaveClass("px-3");
@@ -36,7 +37,7 @@ describe("Button", () => {
 
   it("renders with lg size", () => {
     render(<Button size="lg">Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("h-11");
     expect(button).toHaveClass("px-8");
@@ -54,7 +55,7 @@ describe("Button", () => {
     const handleClick = jest.fn();
     const user = userEvent.setup()
     render(<Button onClick={handleClick}>Click me</Button>);
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     await user.click(button);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
@@ -66,8 +67,23 @@ describe("Button", () => {
         Click me
       </Button>
     );
-    const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByRole("button");
     userEvent.click(button);
     expect(handleClick).not.toHaveBeenCalled();
   });
+
+  it("renders with additional className", () => {
+    render(<Button className="test-class">Click me</Button>)
+    const element = screen.getByRole("button")
+
+    expect(element).toHaveClass("test-class")
+  })
+
+  it("renders with forwarded ref", () => {
+    const ref = React.createRef<HTMLButtonElement>();
+    render(<Button ref={ref}>Click Me</Button>);
+    const element = screen.getByRole("button");
+
+    expect(element).toBe(ref.current)
+  })
 });
