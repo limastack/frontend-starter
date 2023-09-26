d886ccfba656a4f23da5404857630ae073134cf3
Renê Lima <rene.exe@gmail.com>
2023-09-25 22:09:29 -0300

    Calling HeaderTemplate on layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Link } from "@/components/atoms/link";
 
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-start justify-start p-24 bg-slate-400"
+      className="flex min-h-screen flex-col items-start justify-start p-24 bg-slate-800"
       data-testid="main"
     >
 
