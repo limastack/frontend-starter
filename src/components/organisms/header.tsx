e553e0ab8f9f350df4ec63c65b051e985865ee9b
Renê Lima <rene.exe@gmail.com>
2023-09-26 22:12:54 -0300

    Updating header organismo to use navbarMobile

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "../atoms/link";
 import Navbar, { NavbarProps } from "../molecules/navbar";
 import { cn } from "@/lib/utils";
-import Hamburguer from "../atoms/hamburguer";
+import NavbarMobile from "../molecules/navbarMobile";
 
 export interface HeaderProps {
     className?: string;
@@ -20,12 +20,12 @@ const Header = (
         navbar
     }: HeaderProps
 ) => {
-    return <div className={cn("fixed w-full top-0 left-0 h-24 px-9 flex items-center justify-between select-none z-50", className)}>
+    return <div className={cn("fixed w-full top-0 left-0 h-24 px-4 md:px-9 flex items-center justify-between select-none z-50", className)}>
         {brand && (
-            <Link href={brand.href} className="font-bold text-white tracking-wider">{brand.label}</Link>
+            <Link href={brand.href} className="font-bold text-white tracking-wider z-50">{brand.label}</Link>
         )}
         <Navbar links={navbar.links} button={navbar.button || undefined} />
-        <Hamburguer />
+        <NavbarMobile links={navbar.links} />        
     </div>;
 }
 
